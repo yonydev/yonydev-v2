3ff4ac9e97a94f85db4c02f4b9ec74bfc71b37ea
Yonatan Rojas <yonatan@bedu.org>
2019-12-26 16:38:55 -0600

    chore: changed extension .js to .tsx in components and pages

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -6,13 +6,10 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-
-
-function SEO({ description, lang, meta, title }) {
+const SEO = ({ description, lang, meta, title }: any) => {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -72,19 +69,6 @@ function SEO({ description, lang, meta, title }) {
       ].concat(meta)}
     />
   )
-}
-
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
 }
 
 export default SEO
