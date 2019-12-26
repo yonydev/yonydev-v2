e30c1c068ebfd10b9e81182fd83948c69190a728
Yonatan Rojas <yonatan@bedu.org>
2019-12-26 16:39:28 -0600

    feat: added interfaces to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,13 +6,16 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./Header/header"
+import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
+interface ILayout {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: ILayout) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -43,10 +46,6 @@ const Layout = ({ children }) => {
       </div>
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
