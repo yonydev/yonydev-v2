3c7c99da1ece0c5f83763fc952966b6a8f939c43
Yonatan Rojas <yonatan@bedu.org>
2019-12-27 23:06:24 -0600

    chore: changed layout name for container

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+import Layout from "../components/conatiner"
 // import SEO from "../components/seo"
 
 const SecondPage = () => (
